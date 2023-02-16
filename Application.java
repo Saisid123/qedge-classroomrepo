7b95bb993f296cd6428237c920d3166d36c81698
Saisid123 <saikumarsiddagoni154@gmail.com>
2023-02-15 18:02:02 -0600

    modified Application.java file by Saikumar Siddagoni

--- Application.java
+++ Application.java
@@ -1,1 +1,30 @@
 Java is a widely used object-oriented programming language and software platform that runs on billions of devices, including notebook computers, mobile devices, gaming consoles, medical devices and many others. The rules and syntax of Java are based on the C and C++ languages.
+
+
+There will be 4 pilars of OOPS.
+
+Features 201 changes done by Saikumar Siddagoni
+
+/* CallingMethodsInSameClass.java
+ *
+ * illustrates how to call static methods a class
+ * from a method in the same class
+ */
+
+public class CallingMethodsInSameClass
+{
+	public static void main(String[] args) {
+		printOne();
+		printOne();
+		printTwo();
+	}
+
+	public static void printOne() {
+		System.out.println("Hello World");
+	}
+
+	public static void printTwo() {
+		printOne();
+		printOne();
+	}
+}
