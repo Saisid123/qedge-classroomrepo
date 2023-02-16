767d6c3e877c23bb8554d6ace13dcae07314024d
Saisid123 <saikumarsiddagoni154@gmail.com>
2023-02-16 17:24:20 -0600

    feature202 changes on Application.java file by Siva

--- Application.java
+++ Application.java
@@ -1,1 +1,52 @@
 Java is a widely used object-oriented programming language and software platform that runs on billions of devices, including notebook computers, mobile devices, gaming consoles, medical devices and many others. The rules and syntax of Java are based on the C and C++ languages.
+#feature202 changes by Siva on services module 08-feb-2023
+
+//sample code to write 100 random ints to a file, 1 per line
+
+import java.io.PrintStream;
+import java.io.IOException;
+import java.io.File;
+
+import java.util.Random;
+
+public class WriteToFile
+{	public static void main(String[] args)
+	{	try
+		{	PrintStream writer = new PrintStream( new File("randInts.txt"));
+			Random r = new Random();
+			final int LIMIT = 100;
+
+			for(int i = 0; i < LIMIT; i++)
+			{	writer.println( r.nextInt() );
+			}
+			writer.close();
+		}
+		catch(IOException e)
+		{	System.out.println("An error occured while trying to write to the file");
+		}
+	}
+}//sample code to write 100 random ints to a file, 1 per line
+
+import java.io.PrintStream;
+import java.io.IOException;
+import java.io.File;
+
+import java.util.Random;
+
+public class WriteToFile
+{	public static void main(String[] args)
+	{	try
+		{	PrintStream writer = new PrintStream( new File("randInts.txt"));
+			Random r = new Random();
+			final int LIMIT = 100;
+
+			for(int i = 0; i < LIMIT; i++)
+			{	writer.println( r.nextInt() );
+			}
+			writer.close();
+		}
+		catch(IOException e)
+		{	System.out.println("An error occured while trying to write to the file");
+		}
+	}
+}
